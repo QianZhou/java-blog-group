210a433881cad7b2d181e2526a8ccdb859169ff6
QIAN ZHOU <awinqian@gmail.com>
2016-01-12 17:24:33 -0600

    15th commit

--- src/main/java/qian/zhou/jbg/service/InitDbService.java
+++ src/main/java/qian/zhou/jbg/service/InitDbService.java
@@ -1,7 +1,8 @@
 package qian.zhou.jbg.service;
 
 import java.util.ArrayList;
-import java.util.Date;
+
+import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -9,15 +10,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import qian.zhou.jbg.entity.Blog;
-import qian.zhou.jbg.entity.Item;
 import qian.zhou.jbg.entity.Role;
 import qian.zhou.jbg.entity.User;
 import qian.zhou.jbg.repository.BlogRepository;
 import qian.zhou.jbg.repository.ItemRepository;
 import qian.zhou.jbg.repository.RoleRepository;
 import qian.zhou.jbg.repository.UserRepository;
-
-import javax.annotation.PostConstruct;
 
 @Transactional
 @Service
@@ -37,44 +35,43 @@ public class InitDbService {
 
 	@PostConstruct
 	public void init() {
+		if (roleRepository.findByName("ROLE_ADMIN") == null) {
+			Role roleUser = new Role();
+			roleUser.setName("ROLE_USER");
+			roleRepository.save(roleUser);
 
-		Role roleUser = new Role();
-		roleUser.setName("ROLE_USER");
-		roleRepository.save(roleUser);
+			Role roleAdmin = new Role();
+			roleAdmin.setName("ROLE_ADMIN");
+			roleRepository.save(roleAdmin);
 
-		Role roleAdmin = new Role();
-		roleAdmin.setName("ROLE_ADMIN");
-		roleRepository.save(roleAdmin);
+			User userAdmin = new User();
+			userAdmin.setEnabled(true);
+			userAdmin.setName("admin");
+			BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+			userAdmin.setPassword(encoder.encode("admin"));
+			ArrayList<Role> roles = new ArrayList<Role>();
+			roles.add(roleAdmin);
+			roles.add(roleUser);
+			userAdmin.setRoles(roles);
+			userRepository.save(userAdmin);
 
-		User userAdmin = new User();
-		userAdmin.setEnabled(true);
-		userAdmin.setName("admin");
-		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-		userAdmin.setPassword(encoder.encode("admin"));
-		ArrayList<Role> roles = new ArrayList<Role>();
-		roles.add(roleAdmin);
-		roles.add(roleUser);
-		userAdmin.setRoles(roles);
-		userRepository.save(userAdmin);
+			Blog blogJavavids = new Blog();
+			blogJavavids.setName("JavaVids");
+			blogJavavids
+					.setUrl("http://feeds.feedburner.com/javavids?format=xml");
+			blogJavavids.setUser(userAdmin);
+			blogRepository.save(blogJavavids);
 
-		Blog blogJavavids = new Blog();
-		blogJavavids.setName("JavaVids");
-		blogJavavids.setUrl("http://feeds.feedburner.com/javavids?format=xml");
-		blogJavavids.setUser(userAdmin);
-		blogRepository.save(blogJavavids);
-
-		/*Item item1 = new Item();
-		item1.setBlog(blogJavavids);
-		item1.setTitle("first");
-		item1.setPublishedDate(new Date());
-		item1.setLink("http://www.javaids.com");
-		itemRepository.save(item1);
-		Item item2 = new Item();
-		item2.setBlog(blogJavavids);
-		item2.setTitle("second");
-		item2.setPublishedDate(new Date());
-		item2.setLink("http://www.javaids.com");
-		itemRepository.save(item2);*/
-
+			/*
+			 * Item item1 = new Item(); item1.setBlog(blogJavavids);
+			 * item1.setTitle("first"); item1.setPublishedDate(new Date());
+			 * item1.setLink("http://www.javaids.com");
+			 * itemRepository.save(item1); Item item2 = new Item();
+			 * item2.setBlog(blogJavavids); item2.setTitle("second");
+			 * item2.setPublishedDate(new Date());
+			 * item2.setLink("http://www.javaids.com");
+			 * itemRepository.save(item2);
+			 */
+		}
 	}
 }
